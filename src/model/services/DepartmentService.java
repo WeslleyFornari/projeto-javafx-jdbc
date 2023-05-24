66400ca3e09ef51d275d6c453b0833893ff7a62b
Weslley Fornari <weslleyfornari@gmail.com>
2023-05-23 21:01:22 -0300

    Configurando acesso ao banco de dados MySQL

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -3,18 +3,18 @@ package model.services;
 import java.util.ArrayList;
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentService {
 	
+	private DepartmentDao depDao = DaoFactory.createDepartmentDao(); //INJECÃO DE DEPENDENCIA
+	
 	public List<Department> findAll(){
 		
-		List<Department> lista = new ArrayList<>();
-		lista.add(new Department(1, "Livros"));
-		lista.add(new Department(2, "Computadores"));
-		lista.add(new Department(3, "Eletronicos"));
+		return depDao.findAll(); // FAZ A BUSCA NO BANCO DE DADOS
 		
-		return lista;
 		
 	}
 	
